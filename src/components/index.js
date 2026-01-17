5f9f51e35528f27e7cc967f1d0e30b1b3f9f2f96
Son Youngjun <dudwnsths1@naver.com>
2026-01-17 20:44:14 +0900

    refactor: Modal 컴포넌트 폴더 위치 변경

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,1 @@
-// export { default as Button } from "./Button/Button";
-// export { default as Header } from "./Header/Header";
-export { default as Modal } from "./Modal/Modal";
+export { default as Modal } from "./workboard-element/Modal";
