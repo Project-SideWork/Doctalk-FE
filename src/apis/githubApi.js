5899dc33e6fffd11ff401f453c6510955042daef
SonYoungJun <dudwnsths1@naver.com>
2026-01-30 13:46:55 +0900

    feat: 깃허브 모든 활동 관련 api 추가

--- src/apis/githubApi.js
+++ src/apis/githubApi.js
@@ -1,6 +1,19 @@
 import { axiosInstanceNoHeader } from "./axiosInstance";
 
-// 프로젝트 내 나의 깃허브 활동 통계 조회
+// 1. 조직 내 신규 레포 생성
+export const createNewRepo = async (projectId) => {
+  try {
+    const res = await axiosInstanceNoHeader.post(
+      `/github/organizaions/repository`,
+    );
+    return res.data.result;
+  } catch (error) {
+    console.error("생성 실패:", error.response?.data || error.message);
+    throw error;
+  }
+};
+
+// 2. 프로젝트 내 나의 깃허브 활동 통계 조회
 export const checkGithubStat = async (projectId) => {
   try {
     const res = await axiosInstanceNoHeader.get(`/github/stat/${projectId}`);
@@ -11,54 +24,7 @@ export const checkGithubStat = async (projectId) => {
   }
 };
 
-// 나에게 리뷰를 요청한 PR 목록 조회
-export const checkRequestedList = async (projectId) => {
-  try {
-    const res = await axiosInstanceNoHeader.get(
-      `/github/prs/requested/${projectId}`,
-    );
-    return res.data.result;
-  } catch (error) {
-    console.error("조회 실패:", error.response?.data || error.message);
-    throw error;
-  }
-};
-
-// 프로젝트 레포지토리의 이슈 목록 조회
-export const checkProjectIssues = async (projectId) => {
-  try {
-    const res = await axiosInstanceNoHeader.get(`/github/issues/${projectId}`);
-    return res.data.result;
-  } catch (error) {
-    console.error("조회 실패:", error.response?.data || error.message);
-    throw error;
-  }
-};
-
-// 프로젝트내 이슈 + PR 관련 이벤트 조회
-export const checkProjectEvent = async (projectId) => {
-  try {
-    const res = await axiosInstanceNoHeader.get(`/github/event/${projectId}`);
-    console.log("test: ", res.data.result);
-    return res.data.result;
-  } catch (error) {
-    console.error("조회 실패:", error.response?.data || error.message);
-    throw error;
-  }
-};
-
-// 조직 내 신규 레포 생성
-export const createNewRepo = async (projectId) => {
-  try {
-    const res = await axiosInstanceNoHeader.post(`/github/event/`);
-    return res.data.result;
-  } catch (error) {
-    console.error("생성 실패:", error.response?.data || error.message);
-    throw error;
-  }
-};
-
-// 레포 PR 리뷰 통계
+// 3. 레포 PR 리뷰 통계
 export const checkRepoStats = async ({ org, repo, prCount = 10 }) => {
   try {
     const res = await axiosInstanceNoHeader.get("/github/repo/stats", {
@@ -71,7 +37,7 @@ export const checkRepoStats = async ({ org, repo, prCount = 10 }) => {
   }
 };
 
-// 리뷰 코멘트 목록 조회
+// 4. 리뷰 코멘트 목록 조회
 export const fetchReviewComments = async ({ org, repo }) => {
   try {
     const res = await axiosInstanceNoHeader.get(
@@ -87,7 +53,7 @@ export const fetchReviewComments = async ({ org, repo }) => {
   }
 };
 
-// 레포의 PR 목록 조회
+// 5. 레포의 PR 목록 조회
 export const fetchRepoPrList = async ({ org, repo }) => {
   try {
     const res = await axiosInstanceNoHeader.get("/github/prs", {
@@ -96,6 +62,51 @@ export const fetchRepoPrList = async ({ org, repo }) => {
     return res.data.result;
   } catch (error) {
     console.error("PR 목록 조회 실패:", error.response?.data || error.message);
+    throw error;
+  }
+};
+
+// 6. 나에게 리뷰를 요청한 PR 목록 조회
+export const checkRequestedList = async (projectId) => {
+  try {
+    const res = await axiosInstanceNoHeader.get(
+      `/github/prs/requested/${projectId}`,
+    );
+    return res.data.result;
+  } catch (error) {
+    console.error("조회 실패:", error.response?.data || error.message);
+    throw error;
+  }
+};
+
+// 7. 내 깃허브 조직 목록 조회
+export const fetchMyOrgList = async () => {
+  try {
+    const res = await axiosInstanceNoHeader.get(`/github/organizations/my`);
+    return res.data.result;
+  } catch (error) {
+    console.error("조회 실패:", error.message);
+  }
+};
+
+// 8. 프로젝트 레포지토리의 이슈 목록 조회
+export const checkProjectIssues = async (projectId) => {
+  try {
+    const res = await axiosInstanceNoHeader.get(`/github/issues/${projectId}`);
+    return res.data.result;
+  } catch (error) {
+    console.error("조회 실패:", error.response?.data || error.message);
+    throw error;
+  }
+};
+
+// 9. 프로젝트내 이슈 + PR 관련 이벤트 조회
+export const checkProjectEvent = async (projectId) => {
+  try {
+    const res = await axiosInstanceNoHeader.get(`/github/event/${projectId}`);
+    return res.data.result;
+  } catch (error) {
+    console.error("조회 실패:", error.response?.data || error.message);
     throw error;
   }
 };
